c54679c83a9b3af6a55960764f97654716200715
Simone Casazza <simone.casazza94@gmail.com>
2024-04-13 13:01:14 +0200

    fix

--- javascript_exercises/script.js
+++ javascript_exercises/script.js
@@ -1,5 +1,5 @@
 function set() {
-    var inp = document.getElementById("t1").innerHTML
+    var inp = document.getElementById("t1").value
     var val = "<span id='1'>" + inp + "</span>"
-    document.getElementById("t1").innerHTML = val
+    document.getElementById("t1").value = val
 }
