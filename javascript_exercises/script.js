f56f3c57dabad83bb7c7a8867028a812ebf391d8
Simone Casazza <simone.casazza94@gmail.com>
2024-04-13 13:31:08 +0200

    try

--- javascript_exercises/script.js
+++ javascript_exercises/script.js
@@ -1,3 +1,9 @@
 function set() {
     document.getElementById("t1").value = document.getElementById("s1").innerText
 }
+
+function check() {
+    var d1 = document.getElementById("d1").value
+    var d2 = document.getElementById("d1").value
+    alert(d1)
+}
